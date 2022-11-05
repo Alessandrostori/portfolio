18bc302be88fbcc62e8b185d3718b46d2497d4f7
Alessandrostori <alessandro.stori1@gmail.com>
2022-11-05 13:26:20 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'https://github.com/Alessandrostori/portfolio/blob/main/styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
